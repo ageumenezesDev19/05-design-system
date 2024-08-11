9cd0b784919e068b256becbdecbc92a2927be2a8
ageu_dev19 <ageumenezes423@gmail.com>
2024-08-11 03:11:32 -0300

    Add more controls to components

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -9,6 +9,13 @@ export default {
     src: 'https://github.com/ageumenezesDev19.png?s=48',
     alt: 'Ageu Menezes',
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
